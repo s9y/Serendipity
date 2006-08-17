5b0e0e29561003893ef2f2d87ea7c0e39afa17be
Tadashi Jokagi <elf@elf.no-ip.org>
2006-08-17 07:13:21 +0000

    * translation updated.

--- plugins/serendipity_event_statistics/lang_ja.inc.php
+++ plugins/serendipity_event_statistics/lang_ja.inc.php
@@ -3,7 +3,7 @@
 /**
  *  @version $Revision$
  *  @author Tadashi Jokagi <elf2000@users.sourceforge.net>
- *  EN-Revision: 690
+ *  EN-Revision: 1381
  */
 
 @define('PLUGIN_EVENT_STATISTICS_NAME', '統計情報');
@@ -62,15 +62,42 @@
 @define('PLUGIN_EVENT_STATISTICS_EXT_OPT1', 'いいえ!');
 @define('PLUGIN_EVENT_STATISTICS_EXT_OPT2', 'はい, ぺーじの下に');
 @define('PLUGIN_EVENT_STATISTICS_EXT_OPT3', 'はい, ページの上に');
-@define('PLUGIN_EVENT_STATISTICS_EXT_VISITORS', 'Nr of vistors');
-@define('PLUGIN_EVENT_STATISTICS_EXT_VISTODAY', 'Nr of vistors today');
-@define('PLUGIN_EVENT_STATISTICS_EXT_VISTOTAL', 'Total nr of vistors');
+@define('PLUGIN_EVENT_STATISTICS_EXT_ALL', '訪問者統計のみを表示するには「いいえ」で設定します');
+@define('PLUGIN_EVENT_STATISTICS_EXT_ALL_DESC', 'すべて表示しますか? (デフォルト: いいえ)');
+@define('PLUGIN_EVENT_STATISTICS_EXT_ALL1', 'いいえ、カウンター以外を隠します。');
+@define('PLUGIN_EVENT_STATISTICS_EXT_ALL2', 'はい、すべての統計を表示します!');
+@define('PLUGIN_EVENT_STATISTICS_EXT_VISITORS', 'Individual counters');
+@define('PLUGIN_EVENT_STATISTICS_EXT_VISTODAY', '本日の訪問者数');
+@define('PLUGIN_EVENT_STATISTICS_EXT_VISTOTAL', '今までの訪問者数');
+@define('PLUGIN_EVENT_STATISTICS_EXT_HITSTODAY', '本日のヒット数');
+@define('PLUGIN_EVENT_STATISTICS_EXT_HITSTOTAL', '今までのヒット数');
 @define('PLUGIN_EVENT_STATISTICS_EXT_VISSINCE', '訪問者の拡張統計の機能は次の日以降のデータを収集しました: ');
+@define('PLUGIN_EVENT_STATISTICS_EXT_COUNTDESC','The hits can go quite high and are purely for indication of pageviews. ヒット数はすべてのページビューか、再描画で更新されます。この数は実際の訪問者数ではありません。');
 @define('PLUGIN_EVENT_STATISTICS_EXT_VISLATEST', '最新の訪問者');
 @define('PLUGIN_EVENT_STATISTICS_EXT_TOPREFS', 'トップリファラー');
 @define('PLUGIN_EVENT_STATISTICS_EXT_TOPREFS_NONE', 'まだリファラーは記録されていません。');
+@define('PLUGIN_EVENT_STATISTICS_EXT_DAYGRAPH', '1 日毎の訪問数');
+@define('PLUGIN_EVENT_STATISTICS_EXT_MONTHGRAPH', '1 月毎の訪問数');
 @define('PLUGIN_EVENT_STATISTICS_OUT_EXT_STATISTICS', '拡張訪問者統計');
+@define('PLUGIN_EVENT_STATISTICS_BANNED_HOSTS1', '有効にし、ボットをカウントしない');
+@define('PLUGIN_EVENT_STATISTICS_BANNED_HOSTS2', '無効にし、ボットをカウントする');
+@define('PLUGIN_EVENT_STATISTICS_BANNED_HOSTS', 'ロボットのカウント保護');
+@define('PLUGIN_EVENT_STATISTICS_BANNED_HOSTS_DESC', '「はい」に設定すると、ロボットをカウントしません。「いいえ」に設定すると、ロボットをカウントします。現在 25 を超えるロボットを禁止します。');
 @define('PLUGIN_EVENT_STATISTICS_BANNED_HOSTS', 'カウントを禁止するブラウザー');
-@define('PLUGIN_EVENT_STATISTICS_BANNED_HOSTS_DESC', '「|」で区切ったカウントから除外されるべきブラウザーを指定します。');
+@define('PLUGIN_EVENT_STATISTICS_BANNED_HOSTS_DESC', '「|」で区切ったカウントから除外されるべきブラウザーを指定します。現在 25 を超えるボットを禁止しています。');
 
-?>
+@define('PLUGIN_EVENT_STATISTICS_SHOW_LASTENTRY', '最後のエントリの日付を表示する');
+@define('PLUGIN_EVENT_STATISTICS_SHOW_ENTRYCOUNT', 'エントリの数を表示する');
+@define('PLUGIN_EVENT_STATISTICS_SHOW_COMMENTCOUNT', 'コメントの数を表示する');
+@define('PLUGIN_EVENT_STATISTICS_SHOW_MONTHVISITORS', '今月の訪問数を表示する');
+@define('PLUGIN_EVENT_STATISTICS_SHOW_CACHETIMEOUT', 'キャッシュのタイムアウト');
+@define('PLUGIN_EVENT_STATISTICS_SHOW_CACHETIMEOUT_DESC', 'How long may the statistics be shown before they get refreshed? Setting this to a high number of minutes will improve performance, but might not reflect the actual data if set too high.');
+@define('PLUGIN_EVENT_STATISTICS_TEXT', '書式テキスト');
+@define('PLUGIN_EVENT_STATISTICS_TEXT_DESC', '数値・テキストのプレースフォルダーとして「%s」を使う');
+@define('PLUGIN_EVENT_STATISTICS_TEXT_LASTENTRY', '最後のエントリ: %s');
+@define('PLUGIN_EVENT_STATISTICS_TEXT_ENTRYCOUNT', '%s 個eのエントリが書かれた');
+@define('PLUGIN_EVENT_STATISTICS_TEXT_COMMENTCOUNT', '%s 個のコメントが作られた');
+@define('PLUGIN_EVENT_STATISTICS_TEXT_MONTHVISITORS', '今月は %s 人の訪問があった');
+
+@define('PLUGIN_EVENT_STATISTICS_SHOW_CURRENTVISITORS', '現在の訪問者の数を表示する (過去 15 分の合計)');
+@define('PLUGIN_EVENT_STATISTICS_TEXT_CURRENTVISITORS', '%s 人の訪問者がオンラインです');
