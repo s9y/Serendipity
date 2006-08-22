3af4ac87497fcab910710d37cae3e334cd378edc
Ivan Cenov <jwalker@users.sourceforge.net>
2006-08-22 11:03:16 +0000

    1) Translated new string constant to BG language.

--- lang/serendipity_lang_bg.inc.php
+++ lang/serendipity_lang_bg.inc.php
@@ -866,4 +866,4 @@
 
 
 @define('INSTALL_PERMALINK_COMMENTSPATH', 'Път до коментарите');
-@define('PERM_SET_CHILD', 'Set the same permissions on all child directories');
+@define('PERM_SET_CHILD', 'Установяване на същите разрешения за всички поддиректории');
