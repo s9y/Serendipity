93a3f97eb76ef1e4725d3ba3cdce4080b0de1e16
Ivan Cenov <jwalker@users.sourceforge.net>
2006-08-28 16:57:01 +0000

    1) BG translation of new text constant.

--- plugins/serendipity_event_bbcode/UTF-8/lang_bg.inc.php
+++ plugins/serendipity_event_bbcode/UTF-8/lang_bg.inc.php
@@ -8,5 +8,6 @@
         @define('PLUGIN_EVENT_BBCODE_NAME',     'Текстово форматиране: BBCode');
         @define('PLUGIN_EVENT_BBCODE_DESC',     'Форматиране на текст (постинг, коментар, HTML поле) с BBcode');
         @define('PLUGIN_EVENT_BBCODE_TRANSFORM', 'Форматирането с <a href="http://www.phpbb.com/phpBB/faq.php?mode=bbcode">BBCode</a> е разрешено');
+        @define('PLUGIN_EVENT_BBCODE_TARGET',   'Използване на target="blank" за връзките ?');
 
 ?>
