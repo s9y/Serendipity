b196058eb39e580e824804b0fd49f9ce550e6b5d
Joseph Tate <jtate@users.sourceforge.net>
2006-10-31 15:55:26 +0000

    Merge from 1.0

--- include/admin/upgrader.inc.php
+++ include/admin/upgrader.inc.php
@@ -273,8 +273,26 @@ if (($showAbort && $serendipity['GET']['action'] == 'ignore') || $serendipity['G
             if ( is_writable($basedir) ) {
                 echo serendipity_upgraderResultDiagnose(S9Y_U_SUCCESS, WRITABLE);
             } else {
-                echo serendipity_upgraderResultDiagnose(S9Y_U_ERROR, NOT_WRITABLE);
-                $showWritableNote = true;
+                $showWritableNote = false;
+                #Figure out if we're set up a little more securely
+                #PATH_SMARTY_COMPILE/
+                #uploads/
+                #archives/
+                #.htaccess
+                #serendipity_config_local.inc.php
+                # For completeness we could test to make sure the directories
+                # really are directories, but that's probably overkill
+                foreach (array('archives/', PATH_SMARTY_COMPILE . '/', 'uploads/', '.htaccess', 'serendipity_config_local.inc.php') as $path) {
+                    if (!is_writeable($basedir . $path)) {
+                        echo serendipity_upgraderResultDiagnose(S9Y_U_ERROR, NOT_WRITABLE);
+                        $showWritableNote = true;
+                        break;
+                    }
+                }
+
+                if (!$showWritableNote) {
+                    echo serendipity_upgraderResultDiagnose(S9Y_U_SUCCESS, WRITABLE);
+                }
             }
      ?></td>
     </tr>
