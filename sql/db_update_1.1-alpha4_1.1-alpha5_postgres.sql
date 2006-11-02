466b067287f0a4b0715712ecc3ea27b5a1a227dc
Garvin Hicking <mail@garv.in>
2006-11-02 11:34:15 +0000

    Fix a pgsql update routine error, thanks to ads from the forums

--- sql/db_update_1.1-alpha4_1.1-alpha5_postgres.sql
+++ sql/db_update_1.1-alpha4_1.1-alpha5_postgres.sql
@@ -1,4 +1,4 @@
-ALTER TABLE {PREFIX}images ADD COLUMN realname varchar(255) not null default '';
-ALTER TABLE {PREFIX}references ADD COLUMN type varchar(128) not null default '';
+ALTER TABLE {PREFIX}images ADD COLUMN realname varchar(255);
+ALTER TABLE {PREFIX}references ADD COLUMN type varchar(128);
 CREATE INDEX reftype_idx ON {PREFIX}references (type);
 
