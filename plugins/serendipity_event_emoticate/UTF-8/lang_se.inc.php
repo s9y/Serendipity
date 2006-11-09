abb10b1df0e7d64f27df8cb5bd929785b8550f30
Garvin Hicking <mail@garv.in>
2006-11-09 08:38:56 +0000

    More swedish translations by crapmaster frmo the forums

---
+++ plugins/serendipity_event_emoticate/UTF-8/lang_se.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *  @version 
+ *  @author Peter Janson <janson.peter@gmail.com>
+ *  SE-Revision: Revision of lang_se.inc.php
+ */
+
+@define('PLUGIN_EVENT_EMOTICATE_NAME', 'Markup: Emoticate');
+@define('PLUGIN_EVENT_EMOTICATE_DESC', 'Gör om standard emoticons till grafiska bilder');
+@define('PLUGIN_EVENT_EMOTICATE_TRANSFORM', 'Standard emoticons som :-) och ;-) konverteras till bilder.');
+@define('PLUGIN_EVENT_EMOTICATE_EXTENSION', 'Filändelse');
+@define('PLUGIN_EVENT_EMOTICATE_EXTENSION_BLAHBLAH', 'Filändelse för dina emoticons. Denna är "case sensitive", dvs se till att skriva stora och små bokstäver rätt.');
+
+?>
