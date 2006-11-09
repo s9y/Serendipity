460d8ad352f5947c54acfd70b57d689a74f4e4c8
Garvin Hicking <mail@garv.in>
2006-11-09 08:38:56 +0000

    More swedish translations by crapmaster frmo the forums

---
+++ plugins/serendipity_event_s9ymarkup/UTF-8/lang_se.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *  @version 
+ *  @author Peter Janson <janson.peter@gmail.com>
+ *  SE-Revision: Revision of lang_se.inc.php
+ */
+
+@define('PLUGIN_EVENT_S9YMARKUP_NAME', 'Markup: Serendipity');
+@define('PLUGIN_EVENT_S9YMARKUP_DESC', 'Applicera grundläggande serendipity markup på inläggstext');
+@define('PLUGIN_EVENT_S9YMARKUP_TRANSFORM', 'Omslutande asterisker markerar text som fetstil (*ord*), understruken text görs med hjälp av _ord_.');
+
+?>
