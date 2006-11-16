0e88f488ce4e203d8dfa242dcf6ad6dbf072a9e8
Ivan Cenov <jwalker@users.sourceforge.net>
2006-11-16 17:39:33 +0000

    1) BG translation of new strings in serendipity_event_nl2br plugin.

--- plugins/serendipity_event_nl2br/UTF-8/lang_bg.inc.php
+++ plugins/serendipity_event_nl2br/UTF-8/lang_bg.inc.php
@@ -7,3 +7,5 @@
 
 @define('PLUGIN_EVENT_NL2BR_NAME',     'Форматиране на текст: NL2BR');
 @define('PLUGIN_EVENT_NL2BR_DESC',     'Заменя новите редове в текста с HTML BR тагове');
+@define('PLUGIN_EVENT_NL2BR_ISOLATE_TAGS', 'Списък на HTML-тагове, в които няма да се използва това форматиране');
+@define('PLUGIN_EVENT_NL2BR_ISOLATE_TAGS_DESC', 'Препоръка: "code,pre,geshi,textarea". Използвайте "," за да укажете повече от един таг. Забележка: Въведените тагове се изчисляват като регулярни изрази.');
