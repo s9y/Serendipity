7206ae2a19d36b366adaae8ddbd0077b6b363a15
Garvin Hicking <mail@garv.in>
2007-01-14 15:21:36 +0000

    BBcode improvements by jez hancock

--- plugins/serendipity_event_bbcode/serendipity_event_bbcode.php
+++ plugins/serendipity_event_bbcode/serendipity_event_bbcode.php
@@ -24,7 +24,7 @@ class serendipity_event_bbcode extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_BBCODE_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Jez Hancock, Garvin Hicking');
-        $propbag->add('version',       '2.07');
+        $propbag->add('version',       '2.08');
         $propbag->add('requirements',  array(
             'serendipity' => '0.8',
             'smarty'      => '2.6.7',
@@ -204,8 +204,8 @@ class serendipity_event_bbcode extends serendipity_event
               '/(?<!\\\\)\[img(?::\w+)?=([0-9]*?)x([0-9]*?)\]' . $pattern_url . '\[\/img(?::\w+)?\]/si'       => "<img width=\"\\1\" height=\"\\2\" src=\"\\3\" alt=\"\\3\" class=\"bb-image\" />",
 
               // [quote]
-              '/(?<!\\\\)\[quote(?::\w+)?\](.*?)\[\/quote(?::\w+)?\]/si'                                      => "<div class=\"bb-code-title\">QUOTE:<div class=\"bb-code\">\\1</div></div>",
-              '/(?<!\\\\)\[quote(?::\w+)?=(?:&quot;|"|\')?(.*?)["\']?(?:&quot;|"|\')?\](.*?)\[\/quote\]/si'   => "<div class=\"bb-code-title\">QUOTE \\1:<div class=\"bb-code\">\\2</div></div>",
+              '/(?<!\\\\)\[quote(?::\w+)?\](.*?)\[\/quote(?::\w+)?\]/si'                                      => "<div class=\"bb-code-title\">QUOTE:</div><div class=\"bb-code\">\\1</div>",
+              '/(?<!\\\\)\[quote(?::\w+)?=(?:&quot;|"|\')?(.*?)["\']?(?:&quot;|"|\')?\](.*?)\[\/quote\]/si'   => "<div class=\"bb-code-title\">QUOTE \\1:</div><div class=\"bb-code\">\\2</div>",
 
               // [list]
               '/(?<!\\\\)(?:\s*<br\s*\/?>\s*)?\[\*(?::\w+)?\](.*?)(?=(?:\s*<br\s*\/?>\s*)?\[\*|(?:\s*<br\s*\/?>\s*)?\[\/?list)/si' => "\n<li class=\"bb-listitem\">\\1</li>",
@@ -286,6 +286,9 @@ class serendipity_event_bbcode extends serendipity_event
     font-family: courier, "courier new";
     background-color: #DDDDDD;
     padding: 10px;
+    white-space: nowrap;
+    overflow: auto;
+    max-height: 24em;
 }
 .bb-list-ordered-d {
     list-style-type: decimal;
