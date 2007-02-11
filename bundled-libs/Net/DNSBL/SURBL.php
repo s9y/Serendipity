1d48d31115ad187b91dd8651bed5294a6381c474
Sebastian Nohn <sebastian@nohn.net>
2007-02-11 16:47:44 +0000

    * upgrade to latest net_dnsbl (incl. dependencies)

--- bundled-libs/Net/DNSBL/SURBL.php
+++ bundled-libs/Net/DNSBL/SURBL.php
@@ -1,22 +1,39 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-// +----------------------------------------------------------------------+
-// | PEAR::Net_DNSBL_SURBL                                                |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 2004 Sebastian Nohn <sebastian@nohn.net>               |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.php.net/license/3_0.txt.                                  |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Sebastian Nohn <sebastian@nohn.net>                         |
-// +----------------------------------------------------------------------+
-//
-// $Id: SURBL.php,v 1.4 2004/12/02 14:23:51 nohn Exp $
+
+/**
+ * PEAR::Net_DNSBL
+ *
+ * This class acts as interface to generic Realtime Blocking Lists
+ * (RBL)
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * Net_DNSBL looks up an supplied host if it's listed in 1-n supplied
+ * Blacklists
+ *
+ * @category   Net
+ * @package    DNSBL
+ * @author     Sebastian Nohn <sebastian@nohn.net>
+ * @author     Ammar Ibrahim <fixxme@fixme.com>
+ * @copyright  2004-2007 Sebastian Nohn <sebastian@nohn.net>
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    CVS: $Id: SURBL.php,v 1.4 2006/12/25 10:40:59 nohn Exp $
+ * @link       http://pear.php.net/package/Net_DNSBL
+ * @see        Net_DNS
+ * @since      File available since Release 1.0.0
+ */
+
+require_once 'Cache/Lite.php';
+require_once 'HTTP/Request.php';
+require_once 'Net/CheckIP.php';
+require_once 'Net/DNSBL.php';
 
 /**
  * PEAR::Net_DNSBL_SURBL
@@ -28,13 +45,9 @@
  *
  * @author  Sebastian Nohn <sebastian@nohn.net>
  * @package Net_DNSBL
- * @license http://www.php.net/license/3_0.txt
- * @version 0.5.4
+ * @license http://www.php.net/license/3_01.txt
+ * @version 1.2.0
  */
-require_once dirname(__FILE__) . '/../../Cache/Lite.php';
-require_once dirname(__FILE__) . '/../../HTTP/Request.php';
-require_once dirname(__FILE__) . '/../CheckIP.php';
-require_once dirname(__FILE__) . '/../DNSBL.php';
 
 class Net_DNSBL_SURBL extends Net_DNSBL {
 
@@ -139,13 +152,13 @@ class Net_DNSBL_SURBL extends Net_DNSBL {
                 array_shift($host_elements);
             } // while
             $host_3_elements = implode('.', $host_elements);
-
+            
             $host_elements = explode('.', $host);
             while (count($host_elements) > 2) {
                 array_shift($host_elements);
             } // while
             $host_2_elements = implode('.', $host_elements);
-
+            
             // (3b) IS_FQDN Check if is in "CC-2-level-TLD"
             if ($this->isDoubleCcTld($host_2_elements)) {
                 // (3b1) IS_IN_2LEVEL: we want the last three names
@@ -159,6 +172,6 @@ class Net_DNSBL_SURBL extends Net_DNSBL {
         $host      .= '.'.$blacklist;
         return $host;
     } // function
-
+    
 } // class
 ?>
