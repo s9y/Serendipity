18498ddf57a4b5e63e5bfd5fcbd511a170b4885a
Garvin Hicking <mail@garv.in>
2007-03-11 13:34:15 +0000

    Add missing constant

--- plugins/serendipity_event_livesearch/lang_en.inc.php
+++ plugins/serendipity_event_livesearch/lang_en.inc.php
@@ -9,5 +9,5 @@
 @define('PLUGIN_EVENT_LIVESEARCH_NAME', 'Livesearch');
 @define('PLUGIN_EVENT_LIVESEARCH_DESC', 'Enhances the internal search function by adding live-updates on keypress (JavaScript)');
 @define('PLUGIN_EVENT_LIVESEARCH_WAIT', 'Wait, sending request...');
-
+@define('PLUGIN_EVENT_LIVESEARCH_NOTFOUND', 'No entries match your query');
 ?>
