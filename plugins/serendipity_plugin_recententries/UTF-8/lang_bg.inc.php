066118ddbba762d8cd48c46de326a1bc2cf125ae
Ivan Cenov <jwalker@users.sourceforge.net>
2007-06-28 06:20:44 +0000

    1) BG translation for new texts constants.

--- plugins/serendipity_plugin_recententries/UTF-8/lang_bg.inc.php
+++ plugins/serendipity_plugin_recententries/UTF-8/lang_bg.inc.php
@@ -13,3 +13,5 @@
     @define('PLUGIN_RECENTENTRIES_NUMBER_FROM_DESC', 'Само последните постинги, които не са показани на заглавната страница ще бъдат показани. (По подразбиране: последните ' . $serendipity['fetchLimit'] . ' ще бъдат прескочени)');
     @define('PLUGIN_RECENTENTRIES_NUMBER_FROM_RADIO_ALL', 'Покажи всички');
     @define('PLUGIN_RECENTENTRIES_NUMBER_FROM_RADIO_RECENT', 'Прескочи показаните на заглавната страница');
+    @define('PLUGIN_RECENTENTRIES_RANDOMIZE', 'Показване на случайни статии');
+    @define('PLUGIN_RECENTENTRIES_RANDOMIZE_DESC', 'Ако е установено, показва случайно избрани статии.');
