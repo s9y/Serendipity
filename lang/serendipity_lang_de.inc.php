1f40fcc28354583b5dbca4c0fafd52ba9aa0a909
Garvin Hicking <mail@garv.in>
2007-08-07 08:17:21 +0000

    German language change

--- lang/serendipity_lang_de.inc.php
+++ lang/serendipity_lang_de.inc.php
@@ -68,7 +68,7 @@
 @define('NO_COMMENTS', 'Noch keine Kommentare');
 @define('POSTED_BY', 'Geschrieben von');
 @define('ON', 'am');
-@define('A_NEW_COMMENT_BLAHBLAH', 'Im Blog %s wurde ein neuer Kommentar zu Eintrag #%s abgegeben!');
+@define('A_NEW_COMMENT_BLAHBLAH', 'Im Blog %s wurde ein neuer Kommentar zu Eintrag "%s" abgegeben!');
 @define('NO_CATEGORY', 'Keine Kategorie');
 @define('ENTRY_BODY', 'Eintrag');
 @define('EXTENDED_BODY', 'Erweiterter Eintrag');
