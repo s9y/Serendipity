879b4df57873cbf7fe82d99b63523ba7ed16b68e
Ivan Cenov <jwalker@users.sourceforge.net>
2007-12-01 08:46:08 +0000

    1) BG translattion of new text constants in serendipity_event_mailer and serendipity_event_textile.

--- plugins/serendipity_event_textile/lang_bg.inc.php
+++ plugins/serendipity_event_textile/lang_bg.inc.php
@@ -3,8 +3,11 @@
 /**
  *  @version $Revision$
  *  @author Ivan Cenov jwalker@hotmail.bg
- */
+ *  EN-Revision: 2034
+*/
 
     @define('PLUGIN_EVENT_TEXTILE_NAME', 'Форматиране на текст: Textile');
     @define('PLUGIN_EVENT_TEXTILE_DESC', 'Форматиране на текст чрез Textile конвертор.');
     @define('PLUGIN_EVENT_TEXTILE_TRANSFORM', 'Форматиране <a href="http://www.textism.com/tools/textile/">Textile</a> е разрешено');
+    @define('PLUGIN_EVENT_TEXTILE_VERSION', 'Версия на Textile');
+    @define('PLUGIN_EVENT_TEXTILE_VERSION_DESCRIPTION', 'Коя версия на Textile искате да използвате?');
