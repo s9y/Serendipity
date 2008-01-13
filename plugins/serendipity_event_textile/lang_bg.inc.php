87499c63754944bf5d4bb19b3d70f82c66080a68
Ivan Cenov <jwalker@users.sourceforge.net>
2008-01-13 19:52:42 +0000

    1) BG translation of several strings.

--- plugins/serendipity_event_textile/lang_bg.inc.php
+++ plugins/serendipity_event_textile/lang_bg.inc.php
@@ -3,7 +3,7 @@
 /**
  *  @version $Revision$
  *  @author Ivan Cenov jwalker@hotmail.bg
- *  EN-Revision: 2034
+ *  EN-Revision: 2084
 */
 
     @define('PLUGIN_EVENT_TEXTILE_NAME', 'Форматиране на текст: Textile');
@@ -11,3 +11,5 @@
     @define('PLUGIN_EVENT_TEXTILE_TRANSFORM', 'Форматиране <a href="http://www.textism.com/tools/textile/">Textile</a> е разрешено');
     @define('PLUGIN_EVENT_TEXTILE_VERSION', 'Версия на Textile');
     @define('PLUGIN_EVENT_TEXTILE_VERSION_DESCRIPTION', 'Коя версия на Textile искате да използвате?');
+    @define('PLUGIN_EVENT_TEXTILE_UNESCAPE', 'Конвертиране на HTML кавички в истински кавички?');
+    @define('PLUGIN_EVENT_TEXTILE_UNESCAPE_DESC', 'Ако е позволено, HTML-Quote-Entities (&quot;) ще бъдат конвертирани в истински кавички. Полезно е, акп желаете да използвате Textile форматиране в коментарите.');
