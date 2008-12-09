40960bb8bde2709a4d221e9eda26e13d98e2c2ea
Garvin Hicking <mail@garv.in>
2008-12-09 10:08:29 +0000

    do not use count(distinct) for SQLite

--- include/plugin_internal.inc.php
+++ include/plugin_internal.inc.php
@@ -473,6 +473,12 @@ class serendipity_archives_plugin extends serendipity_plugin {
 
         echo '<ul>' . "\n";
         
+        if ($serendipity['dbType'] == 'sqlite' || $serendipity['dbType'] == 'sqlite3') {
+            $dist_sql = 'count(e.id) AS orderkey';
+        } else {
+            $dist_sql = 'count(DISTINCT e.id) AS orderkey';
+        }
+        
         for($x = 0; $x < $max_x; $x++) {
             $current_ts = $ts;
             switch($freq) {
@@ -552,7 +558,7 @@ class serendipity_archives_plugin extends serendipity_plugin {
                     '',
                     false,
                     true,
-                    'count(DISTINCT e.id) AS orderkey',
+                    $dist_sql,
                     '',
                     'single',
                     false, $category_set // the joins used
