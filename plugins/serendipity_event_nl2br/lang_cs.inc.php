a1a7fffaefe02ecc8dfecf877daaa63505357e92
Vladimir Ajgl <fugazza@users.sourceforge.net>
2009-08-15 08:38:47 +0000

    Fix of badly formated language files

--- plugins/serendipity_event_nl2br/lang_cs.inc.php
+++ plugins/serendipity_event_nl2br/lang_cs.inc.php
@@ -1,21 +1,19 @@
-<?php # lang_cs.inc.php 1501.0 2009-05-06 20:12:59 VladaAjgl $
+<?php # lang_cs.inc.php 1501.1 2009-08-15 10:29:04 VladaAjgl $
 
 /**
- *  @version 1501.0
+ *  @version 1501.1
  *  @author Vladimír Ajgl <vlada@ajgl.cz>
  *  EN-Revision: Revision of lang_en.inc.php
  *  Translated on 2007/11/30
  *  @author Vladimír Ajgl <vlada@ajgl.cz>
  *  @revisionDate 2009/05/06
+ *  @author Vladimír Ajgl <vlada@ajgl.cz>
+ *  @revisionDate 2009/08/15
  */
 
 @define('PLUGIN_EVENT_NL2BR_NAME',              'Markup: NL2BR');
 @define('PLUGIN_EVENT_NL2BR_DESC',              'Mìní znaky konce øádku na tagy <br />');
 @define('PLUGIN_EVENT_NL2BR_ISOLATE_TAGS',      'Seznam html tagù, uvnitø kterých nemají být konce øádkù nahrazovány.');
 @define('PLUGIN_EVENT_NL2BR_ISOLATE_TAGS_DESC', 'Návrhy: "code,pre,geshi,textarea". Názvy tagù oddìlujte èárkou. Tip: Zadávané tagy jsou vyhodnocovány jako regulární výraz.');
-?>
-
-
-// Next lines were translated on 2009/05/06
 @define('PLUGIN_EVENT_NL2BR_PTAGS',              'Použít tagy &lt;p&gt;');
 @define('PLUGIN_EVENT_NL2BR_PTAGS_DESC',              'Vkládat tagy &lt;p&gt; místo tagù &lt;br /&gt;');
