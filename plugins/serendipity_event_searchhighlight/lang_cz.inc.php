b7dff39d5d8798ad5ca147838a5632ac2cc3109d
Vladimir Ajgl <fugazza@users.sourceforge.net>
2010-10-12 20:39:24 +0000

    Update of czech language files, czech documentation added

--- plugins/serendipity_event_searchhighlight/lang_cz.inc.php
+++ plugins/serendipity_event_searchhighlight/lang_cz.inc.php
@@ -1,12 +1,16 @@
-<?php # $Id: lang_cz.inc.php 1381 2007-12-15 10:14:56Z elf2000 $
+<?php # lang_cz.inc.php 1381.0 2010-10-06 18:59:26 VladaAjgl $
 
 /**
- *  @version $Revision: 1381 $
+ *  @version 1381.0
  *  @author Vladimír Ajgl <vlada@ajgl.cz>
  *  EN-Revision: Revision of lang_en.inc.php
+ *  @author Vladimír Ajgl <vlada@ajgl.cz>
+ *  @revisionDate 2010/10/06
  */
 
 @define('PLUGIN_EVENT_SEARCHHIGHLIGHT_NAME',     'Zvýraznìní vyhledávaných øetìzcù');
 @define('PLUGIN_EVENT_SEARCHHIGHLIGHT_DESC',     'Zvýrazòuje øetìzce hledané v odkazujícím vyhledávaèi.');
 
-?>
+// Next lines were translated on 2010/10/06
+@define('PLUGIN_EVENT_SEARCHHIGHLIGHT_STATICPAGE',     'Statické stránky');
+@define('PLUGIN_EVENT_SEARCHHIGHLIGHT_STATICPAGE_DESC',     'Zvýrazòovat (hledat) øetìzce ve statických stránkách?');
