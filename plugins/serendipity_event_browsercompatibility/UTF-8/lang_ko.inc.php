7fa9ecde6d7817a17b620cebaace414c45604bff
Garvin Hicking <mail@garv.in>
2011-08-13 09:55:33 +0000

    IE6 is dead

--- plugins/serendipity_event_browsercompatibility/UTF-8/lang_ko.inc.php
+++
@@ -1,8 +0,0 @@
-<?php # $Id$
-# Translated by: Wesley Hwang-Chung <wesley96@gmail.com> 
-# (c) 2005 http://www.tool-box.info/
-
-        @define('PLUGIN_EVENT_BROWSERCOMPATIBILITY_TITLE', '브라우저 호환성');
-        @define('PLUGIN_EVENT_BROWSERCOMPATIBILITY_DESC', '브라우저 호환성을 최대화하기 위해 다른 종류의 (CSS) 방식을 사용함');
-
-?>
