38b3bb9c08f340e0426054f7ecb85930d4905f3e
Garvin Hicking <blog@garv.in>
2012-01-06 10:49:11 +0100

    dutch translation update by eric

---
+++ plugins/serendipity_event_emoticate/lang_nl.inc.php
@@ -0,0 +1,14 @@
+<?php # $Id: lang_nl.inc.php 1381 2006-08-15 10:14:56Z elf2000 $
+
+/**
+ *  @version $Revision: 1381 $
+ *  @author Eric Lopes Cardozo ericl@ericlopescardozo.nl
+ *  NL-Revision: Revision of lang_nl.inc.php
+ */
+
+@define('PLUGIN_EVENT_EMOTICATE_NAME', 'Opmaak: Emoticate');
+@define('PLUGIN_EVENT_EMOTICATE_DESC', 'Converteer standaard emoticons naar afbeeldingen');
+@define('PLUGIN_EVENT_EMOTICATE_TRANSFORM', 'Standaad emoticons zoals :-) en ;-) worden geconverteerd naar afbeeldingen.');
+@define('PLUGIN_EVENT_EMOTICATE_EXTENSION', 'Bestandsextensie');
+@define('PLUGIN_EVENT_EMOTICATE_EXTENSION_BLAHBLAH', 'De bestandsextensie van je emoticons. Dit is case sensitive.');
+?>
---
+++ plugins/serendipity_event_s9ymarkup/UTF-8/lang_nl.inc.php
@@ -0,0 +1,13 @@
+<?php # $Id: lang_nl.inc.php 1381 2006-08-15 10:14:56Z elf2000 $
+
+/**
+ *  @version $Revision: 1381 $
+ *  @author Eric Lopes Cardozo <eric@ericlopescardozo.com>
+ *  NL-Revision: Revision of lang_nl.inc.php
+ */
+ 
+@define('PLUGIN_EVENT_S9YMARKUP_NAME', 'Tekstopmaak: Serendipity');
+@define('PLUGIN_EVENT_S9YMARKUP_DESC', 'Standaard Serendipity tekstopmaak doorvoeren');
+@define('PLUGIN_EVENT_S9YMARKUP_TRANSFORM', 'Woorden vet markeren d.m.v. *woord*, onderstrepen d.m.v  _woord_.');
+
+?>
