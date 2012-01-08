5de3096865bf109f3852e1aec8824ea37d0ae27d
Matthias Mees <mm@yellowled.de>
2012-01-08 21:25:20 +0100

    Backported Czech translation.

---
+++ templates/2k11/lang_cz.inc.php
@@ -0,0 +1,27 @@
+<?php # lang_cz.inc.php 1.0 2012-01-08 18:49:05 VladaAjgl $
+
+/**
+ *  @version 1.0
+ *  @author Vladimír Ajgl <vlada@ajgl.cz>
+ *  @translated 2012/01/08
+ */
+@define('TWOK11_HEADER_IMG','Pou¾ít banner - obrázek v záhlaví stránky? (1120 pixelù ¹iroký; ponechte prázdné políèko a nebude se pou¾ívat ¾ádný banner)');
+@define('TWOK11_USE_CORENAV', 'Pou¾ít globální navigaci?');
+@define('TWOK11_WEBFONTS', 'Pou¾ít webfont hostovaný u Googlu?');
+@define('TWOK11_NOWEBFONT', 'Nevkládat ¾ádný webfont');
+@define('TWOK11_USERSTYLES', 'Vkládat user.css? (Je tøeba vytvoøit tento soubor v adresáøi ¹ablony. Generátor souborù na: http://yellowled.github.com)');
+// Lang constants
+@define('TWOK11_NAV_TITLE', 'Navigace');
+@define('TWOK11_PAG_TITLE', 'Stránkování');
+@define('TWOK11_PLINK_TEXT', 'Odkaz');
+@define('TWOK11_PLINK_TITLE', 'Stálý odkaz na tento komentáø');
+@define('TWOK11_PLACE_SEARCH', 'Vyhledat výraz(y)');
+@define('TWOK11_PLACE_NAME', 'napø. Honza Králík');
+@define('TWOK11_PLACE_MAIL', 'napø. mail@priklad.cz');
+@define('TWOK11_PLACE_URL', 'napø. http://priklad.cz');
+@define('TWOK11_PLACE_MESSAGE', 'Co máte na jazyku?');
+@define('TWOK11_SEND_MAIL', 'Poslat');
+@define('TWOK11_TWEET_THIS', 'Sdílet pøíspìvek na Twitteru');
+@define('TWOK11_DENT_THIS', 'Sdílet pøíspìvek na Identice');
+@define('TWOK11_SHORT_URL', 'Krátké URL');
+@define('TWOK11_SHORT_URL_HINT', 'Tento odkaz není my¹len ke klikání. Obsahuje krátkou URL adresu k tomuto pøíspìvku. Mù¾ete ji pou¾ít k odkazování na tento pøíspìvek. Zkopírujte kliknutím pravým tlaèítkem a následnì kliknìte na "Kopírovat odkaz" v Internet Exporeru nebo "Kopírovat umístìní odkazu" v Mozille.');
