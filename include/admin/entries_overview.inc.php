cccd3aca578337fb76f1e608a9171426c79d49bd
Ian <aristophian@googlemail.com>
2012-06-01 20:24:08 +0200

    no need for assignment

--- include/admin/entries_overview.inc.php
+++ include/admin/entries_overview.inc.php
@@ -8,12 +8,8 @@ if (!is_object($serendipity['smarty'])) {
     serendipity_smarty_init();
 }
 
-$serendipity['smarty']->assign($data);
-
 $tfile = dirname(__FILE__) . "/tpl/entries_overview.inc.tpl";
-
-$content = $serendipity['smarty']->fetch('file:'. $tfile); // short notation with Smarty3 in S9y 1.7 and up
-
+$content = $serendipity['smarty']->fetch('file:'. $tfile);
 echo $content;
 
 /* vim: set sts=4 ts=4 expandtab : */
--- include/admin/export.inc.php
+++ include/admin/export.inc.php
@@ -1,6 +1,4 @@
 <?php # $Id$
-# Copyright (c) 2003-2005, Jannis Hermanns (on behalf the Serendipity Developer Team)
-# All rights reserved.  See LICENSE file for licensing details
 
 if (IN_serendipity !== true) {
     die ("Don't hack!");
@@ -10,13 +8,8 @@ if (!is_object($serendipity['smarty'])) {
     serendipity_smarty_init();
 }
 
-$serendipity['smarty']->assign($data);
-
 $tfile = dirname(__FILE__) . "/tpl/export.inc.tpl";
-
-$content = $serendipity['smarty']->fetch('file:'. $tfile); // short notation with Smarty3 in S9y 1.7 and up
-
+$content = $serendipity['smarty']->fetch('file:'. $tfile);
 echo $content;
 
 /* vim: set sts=4 ts=4 expandtab : */
-?>
