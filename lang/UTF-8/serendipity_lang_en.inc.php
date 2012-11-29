28a30ce0504e0c8f92217329c61c2866a4d7743f
Ian <aristophian@googlemail.com>
2012-11-29 14:00:41 +0100

    added new constant PLAIN_ASCII_NAMES

--- lang/UTF-8/serendipity_lang_en.inc.php
+++ lang/UTF-8/serendipity_lang_en.inc.php
@@ -970,3 +970,6 @@
 @define('SUMMARY', 'Summary');
 @define('ARCHIVE_SORT_STABLE', 'Stable Archives');
 @define('ARCHIVE_SORT_STABLE_DESC', 'Sort the archive-pages descending, so they are stable and search-crawler do not have to reindex them.');
+
+// added 2012-11-29
+@define('PLAIN_ASCII_NAMES', '(no special characters, umlauts)');
