a9e94e9569640c39510ed8d37357cf079b399383
Ian <aristophian@googlemail.com>
2012-12-03 16:53:57 +0100

    fixed typos in 1f9ed0fa2a

--- include/functions_images.inc.php
+++ include/functions_images.inc.php
@@ -3296,27 +3296,27 @@ function serendipity_moveMediaDirectory($oldDir, $newDir, $type = 'dir', $item_i
     if ($type == 'dir') {
         if (!is_dir($real_oldDir)) {
             echo '<span class="msg_error">';
-            printf(ERROR_FILE_NOT_EXISTS, . '<span class="block_level">' . $oldDir . '</span>');
+            printf(ERROR_FILE_NOT_EXISTS, '<span class="block_level">' . $oldDir . '</span>');
             echo '</span>';
             return false;
         }
 
         if (is_dir($real_newDir)) {
             echo '<span class="msg_error">';
-            printf(ERROR_FILE_EXISTS, . '<span class="block_level">' . $newDir . '</span>');
+            printf(ERROR_FILE_EXISTS, '<span class="block_level">' . $newDir . '</span>');
             echo '</span>';
             return false;
         }
 
         if (!rename($real_oldDir, $real_newDir)) {
             echo '<span class="msg_error">';
-            printf(MEDIA_DIRECTORY_MOVE_ERROR, . '<span class="block_level">' . $newDir . '</span>');
+            printf(MEDIA_DIRECTORY_MOVE_ERROR, '<span class="block_level">' . $newDir . '</span>');
             echo '</span>';
             return false;
         }
 
         echo '<span class="msg_success">';
-        printf(MEDIA_DIRECTORY_MOVED, . '<span class="block_level">' . $newDir . '</span>');
+        printf(MEDIA_DIRECTORY_MOVED, '<span class="block_level">' . $newDir . '</span>');
         echo '</span>';
 
         $dirs = serendipity_db_query("SELECT id, path
