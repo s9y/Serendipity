a4e14b62c2b28530aa52f83c7129de0704b0fead
Garvin Hicking <blog@garv.in>
2013-01-19 11:35:05 +0100

    Label as 1.7-rc1

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -45,11 +45,11 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version']         = '1.7-alpha1';
+$serendipity['version']         = '1.7-rc1';
 
 // Setting this to 'false' will enable debugging output. All alpa/beta/cvs snapshot versions will emit debug information by default. To increase the debug level (to enable Smarty debugging), set this flag to 'debug'.
 if (!isset($serendipity['production'])) {
-    $serendipity['production']      = (preg_match('@\-(alpha|beta|cvs)@', $serendipity['version']) ? false : true);
+    $serendipity['production']      = (preg_match('@\-(alpha|beta|cvs|rc)@', $serendipity['version']) ? false : true);
 }
 
 // Set error reporting
