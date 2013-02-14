56679cf481faf5149b2b5b18df9b755a1a69678b
Matthias Mees <mm@yellowled.de>
2013-02-14 22:11:25 +0100

    Added AccessifyHTML5 plugin to take care of WAI-ARIA roles.

--- templates/2k11/admin/js/2k11.admin.js
+++ templates/2k11/admin/js/2k11.admin.js
@@ -1,1 +1,43 @@
-jQuery(document).ready(function($) { });
+(function($) {
+    // https://github.com/yatil/accessifyhtml5.js
+    var AccessifyHTML5 = function (defaults) {
+        var fixes = {
+            'article'       : { 'role':          'article'       },
+            'aside'         : { 'role':          'complementary' },
+            'nav'           : { 'role':          'navigation'    },
+            'output'        : { 'aria-live':     'polite'        },
+            'section'       : { 'role':          'region'        },
+            '[required]'    : { 'aria-required': 'true'          }
+        };
+
+        if (defaults) {
+            if (defaults.header) {
+                fixes[defaults.header] = {
+                    'role': 'banner'
+                };
+            }
+            if (defaults.footer) {
+                fixes[defaults.footer] = {
+                    'role': 'contentinfo'
+                }
+            }
+            if (defaults.main) {
+                fixes[defaults.main] = {
+                    'role': 'main'
+                }
+            }
+        }
+
+        $.each(fixes,
+            function(index, item) {
+                $(index).attr(item);
+            }
+        );
+    };
+
+    AccessifyHTML5({
+        header: '#top>div>div',
+        main:   '#main',
+        footer: '#meta'
+    });
+})(jQuery);
