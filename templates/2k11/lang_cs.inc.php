4affeb31076b136be3e362a8f6a35bc300d72b9d
Matthias Mees <mm@yellowled.de>
2013-03-31 22:03:24 +0200

    Backported 2k11 translations from master branch.

--- templates/2k11/lang_cs.inc.php
+++ templates/2k11/lang_cs.inc.php
@@ -1,11 +1,13 @@
-<?php # lang_cs.inc.php 1.1 2013-03-03 12:45:34 VladaAjgl $
+<?php # lang_cs.inc.php 1.2 2013-03-31 13:06:08 VladaAjgl $
 
 /**
- *  @version 1.1
+ *  @version 1.2
  *  @author Vladimír Ajgl <vlada@ajgl.cz>
  *  @translated 2012/01/08
  *  @author Vladimír Ajgl <vlada@ajgl.cz>
  *  @revisionDate 2013/03/03
+ *  @author Vladimír Ajgl <vlada@ajgl.cz>
+ *  @revisionDate 2013/03/31
  */
 @define('TWOK11_HEADER_IMG','Použít banner - obrázek v záhlaví stránky? (1120 pixelù široký; ponechte prázdné políèko a nebude se používat žádný banner)');
 @define('TWOK11_USE_CORENAV', 'Použít globální navigaci?');
@@ -30,9 +32,16 @@
 @define('TWOK11_SHORT_URL_HINT', 'Tento odkaz není myšlen ke klikání. Obsahuje krátkou URL adresu k tomuto pøíspìvku. Mùžete ji použít k odkazování na tento pøíspìvek. Zkopírujte kliknutím pravým tlaèítkem a následnì kliknìte na Kopírovat odkaz v Internet Exporeru nebo Kopírovat umístìní odkazu v Mozille.');
 
 // Next lines were translated on 2013/03/03
+
 @define('TWOK11_REFCOMMENTS', 'Pøidat ke komntáøùm odkaz na pøíbuzné komentáøe (pokud jsou odpovìdí na jiný komentáø)?');
 @define('TWOK11_SIDEBAR', 'Postranní sloupec');
 @define('TWOK11_EMPTYTRACKBACK', 'Obsah této Odezvy bohužel nemùže být zobrazen.');
 @define('TWOK11_TRACKBACKPREVIEW', 'Ukázat náhled');
 @define('TWOK11_REPLYORIGIN', 'Pùvodní');
 @define('TWOK11_FORMOPTIONS', 'Možnosti formuláøe');
+
+// Next lines were translated on 2013/03/31
+@define('TWOK11_IMGSTYLE', 'Vzhled obrázkù v pøíspìvcích');
+@define('TWOK11_IMGSTYLE_NONE', 'Bez rámeèku');
+@define('TWOK11_IMGSTYLE_BORDER', 'Jednoduchý rámeèek');
+@define('TWOK11_IMGSTYLE_SHADOW', 'Stínovaný rámeèek');
--- templates/2k11/lang_cz.inc.php
+++ templates/2k11/lang_cz.inc.php
@@ -1,11 +1,13 @@
-<?php # lang_cz.inc.php 1.1 2013-03-03 12:45:34 VladaAjgl $
+<?php # lang_cz.inc.php 1.2 2013-03-31 13:05:31 VladaAjgl $
 
 /**
- *  @version 1.1
+ *  @version 1.2
  *  @author Vladimír Ajgl <vlada@ajgl.cz>
  *  @translated 2012/01/08
  *  @author Vladimír Ajgl <vlada@ajgl.cz>
  *  @revisionDate 2013/03/03
+ *  @author Vladimír Ajgl <vlada@ajgl.cz>
+ *  @revisionDate 2013/03/31
  */
 @define('TWOK11_HEADER_IMG','Pou¾ít banner - obrázek v záhlaví stránky? (1120 pixelù ¹iroký; ponechte prázdné políèko a nebude se pou¾ívat ¾ádný banner)');
 @define('TWOK11_USE_CORENAV', 'Pou¾ít globální navigaci?');
@@ -30,9 +32,16 @@
 @define('TWOK11_SHORT_URL_HINT', 'Tento odkaz není my¹len ke klikání. Obsahuje krátkou URL adresu k tomuto pøíspìvku. Mù¾ete ji pou¾ít k odkazování na tento pøíspìvek. Zkopírujte kliknutím pravým tlaèítkem a následnì kliknìte na Kopírovat odkaz v Internet Exporeru nebo Kopírovat umístìní odkazu v Mozille.');
 
 // Next lines were translated on 2013/03/03
+
 @define('TWOK11_REFCOMMENTS', 'Pøidat ke komntáøùm odkaz na pøíbuzné komentáøe (pokud jsou odpovìdí na jiný komentáø)?');
 @define('TWOK11_SIDEBAR', 'Postranní sloupec');
 @define('TWOK11_EMPTYTRACKBACK', 'Obsah této Odezvy bohu¾el nemù¾e být zobrazen.');
 @define('TWOK11_TRACKBACKPREVIEW', 'Ukázat náhled');
 @define('TWOK11_REPLYORIGIN', 'Pùvodní');
 @define('TWOK11_FORMOPTIONS', 'Mo¾nosti formuláøe');
+
+// Next lines were translated on 2013/03/31
+@define('TWOK11_IMGSTYLE', 'Vzhled obrázkù v pøíspìvcích');
+@define('TWOK11_IMGSTYLE_NONE', 'Bez rámeèku');
+@define('TWOK11_IMGSTYLE_BORDER', 'Jednoduchý rámeèek');
+@define('TWOK11_IMGSTYLE_SHADOW', 'Stínovaný rámeèek');
