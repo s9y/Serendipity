f1918837087e5bbf5711eeab58d7d8d7db9d09c2
Garvin Hicking <blog@garv.in>
2013-05-11 15:24:22 +0200

    prepare release

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -45,7 +45,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version']         = '1.7-rc4';
+$serendipity['version']         = '1.7';
 
 // Setting this to 'false' will enable debugging output. All alpa/beta/cvs snapshot versions will emit debug information by default. To increase the debug level (to enable Smarty debugging), set this flag to 'debug'.
 if (!isset($serendipity['production'])) {
