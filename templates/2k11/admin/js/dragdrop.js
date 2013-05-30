ccd4fca6957d2e88f354e72ee251f4d2fef45ddf
Matthias Mees <mm@yellowled.de>
2013-05-30 20:27:32 +0200

    No more noConflict, yay.

--- templates/2k11/admin/js/dragdrop.js
+++ templates/2k11/admin/js/dragdrop.js
@@ -1,14 +1,14 @@
-jQuery("document").ready(function() {
-    jQuery('.pluginmanager_container').sortable(
-        {
+$("document").ready(function() {
+    $('.pluginmanager_container').sortable({
             containerSelector: '.pluginmanager_container',
             group: 'plugins',    // TODO: Distinguish between sidebar and event-plugin-container
             handle: '.pluginmanager_grablet',
+
             onDrop: function ($item, container, _super) {
                 var placement = $item.parents('.pluginmanager_container').data("placement");
                 $item.find('input[name$="placement]"]').val(placement);
                 $item.removeClass("dragged").removeAttr("style")
-                jQuery("body").removeClass("dragging")
+                $("body").removeClass("dragging")
                 $.autoscroll.stop();
             },
             onDragStart: function ($item, container, _super) {
