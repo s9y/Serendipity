9670e3a5c3e707d61a4887cf594e181285b1e032
Ian <aristophian@googlemail.com>
2014-01-10 14:57:04 +0100

    textile - better placement warning

--- plugins/serendipity_event_textile/UTF-8/lang_de.inc.php
+++ plugins/serendipity_event_textile/UTF-8/lang_de.inc.php
@@ -17,7 +17,7 @@
 @define('PLUGIN_EVENT_TEXTILE_UNESCAPE_DESC', 'Wenn aktiviert werden HTML-Quote-Entities (&quot;) in richtige Anführungszeichen umgewandelt. Wird nur benötigt, wenn Textile Auszeichnung in Kommentaren verwendet werden soll, damit Links dargestellt werden können.');
 
 // Next lines were translated on 2014/01/06
-@define('PLUGIN_EVENT_TEXTILE_EXAMPLE_NOTE', 'Achtung: Die besten Resulte werden erzielt, wenn dieses Plugin an der Spitze aller markup Plugins, aber <u>nach</u> emoticate in der Pluginliste steht!');
+@define('PLUGIN_EVENT_TEXTILE_EXAMPLE_NOTE', 'Achtung: Die besten Resulte werden erzielt, wenn dieses Plugin an der Spitze aller Markup Plugins, aber <u>nach</u> Smilies "serendipity_event_emoticate" und Smarty Parsing "serendipity_event_smartymarkup" in der Pluginliste steht! Insbesondere Letzteres kann ansonsten zu potentielle Fehlermeldungen führen.');
 @define('PLUGIN_EVENT_TEXTILE_DOCTYPE', 'Textile: Konvertiere nach HTML5 (nur mit lib3 - benötigt ein HTML5 Template, wie 2k11)');
 @define('PLUGIN_EVENT_TEXTILE_DOCTYPE_DESC', 'Momentan kann PHP-Textile entweder XHTML (default) oder HTML5 Quellcode produzieren. Im HTML5 Modus werden, außer den single-tag Endungen, nur kleinere Änderungen, wie acronym zu abbr und align Attribute in Images zu Klassen vorgenommen.');
 // todo: future 
