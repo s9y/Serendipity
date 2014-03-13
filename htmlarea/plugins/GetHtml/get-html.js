95cddcd6a2a19671daa436fe0e37f0abf10d151b
Ian <aristophian@googlemail.com>
2014-03-13 08:09:46 +0100

    prepare htmlarea with upgraded ckeditor for installer
    References #72

--- htmlarea/plugins/GetHtml/get-html.js
+++
@@ -1,8 +0,0 @@
-/* This compressed file is part of Xinha. For uncompressed sources, forum, and bug reports, go to xinha.org */
-/* This file is part of version 0.95 released Mon, 12 May 2008 17:33:15 +0200 */
-/* The URL of the most recent version of this file is http://svn.xinha.webfactional.com/trunk/plugins/GetHtml/get-html.js */
-function GetHtml(_1){
-_1.config.getHtmlMethod="TransformInnerHTML";
-}
-GetHtml._pluginInfo={name:"GetHtml",version:"1.0",developer:"Nelson Bright",developer_url:"http://www.brightworkweb.com/",sponsor:"",sponsor_url:"",license:"htmlArea"};
-
