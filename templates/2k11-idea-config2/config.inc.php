d414125098dcd0ae8f9158a0e73b6e05f0738608
Garvin Hicking <blog@garv.in>
2014-11-03 10:20:56 +0100

    Remove test template, obsolete

--- templates/2k11-idea-config2/config.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (IN_serendipity !== true) { die ("Don't hack!"); }
-
-@serendipity_plugin_api::load_language(dirname(__FILE__));
-
-$serendipity['smarty']->assign('testvar', 'this is parsed [no api js hook!');
