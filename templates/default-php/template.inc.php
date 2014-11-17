50c42997cfc0e82640f6f8a09efa1034c01b5181
Garvin Hicking <blog@garv.in>
2014-11-17 09:41:48 +0100

    Move default-php and default-xml to spartacus

--- templates/default-php/template.inc.php
+++
@@ -1,7 +0,0 @@
-<?php 
-// THIS FILE IS WORK-IN-PROGRESS. Mostly proof-of-code.
-// Proceed at your own risk. Read the "template_api.inc.php
-// instructions.
-include_once S9Y_INCLUDE_PATH . 'include/template_api.inc.php'; 
-$GLOBALS['template'] = new serendipity_smarty_emulator(); 
-$GLOBALS['serendipity']['smarty'] =& $GLOBALS['template'];
