2944725b6925733846fa9f9de33c1a3fca731a84
Garvin Hicking <blog@garv.in>
2015-01-02 09:26:43 +0100

    language constants

--- lang/UTF-8/serendipity_lang_ru.inc.php
+++ lang/UTF-8/serendipity_lang_ru.inc.php
@@ -1088,3 +1088,4 @@ $i18n_filename_to   = array('_', 'a', 'A', 'b', 'B', 'v', 'V', 'g', 'G', 'd', 'D
 @define('CATEGORY_PLUGIN_SHOWALL', 'Show a link to "All categories"?');
 @define('CATEGORY_PLUGIN_SHOWALL', 'If enabled, a link for the visitor to display the blog with no category restriction will be added.');
 @define('SERENDIPITY_PHPVERSION_FAIL', 'Serendipity requires a PHP version >= %2$s - you are running a lower version (%1$s) and need to upgrade your PHP version. Most providers offer you to switch to newer PHP versions through their admin panels or .htaccess directives.');
+@define('TOGGLE_VIEW', 'Switch category view mode');
