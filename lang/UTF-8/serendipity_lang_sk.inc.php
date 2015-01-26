460dd967442f39100cfaeeb475ac11982390ab60
Garvin Hicking <blog@garv.in>
2015-01-26 09:18:08 +0100

    DASHBAORD -> DASHBOARD

--- lang/UTF-8/serendipity_lang_sk.inc.php
+++ lang/UTF-8/serendipity_lang_sk.inc.php
@@ -1079,4 +1079,4 @@ $i18n_filename_to = array (
 @define('PUBLISH_NOW', 'Publish this entry now (sets current time and date)');
 @define('EDITOR_TAGS', 'Tags');
 @define('EDITOR_NO_TAGS', 'No tags');
-@define('DASHBAORD_ENTRIES', 'In Progress');
+@define('DASHBOARD_ENTRIES', 'In Progress');
--- lang/serendipity_lang_pt_PT.inc.php
+++ lang/serendipity_lang_pt_PT.inc.php
@@ -1100,4 +1100,4 @@
 @define('PUBLISH_NOW', 'Publish this entry now (sets current time and date)');
 @define('EDITOR_TAGS', 'Tags');
 @define('EDITOR_NO_TAGS', 'No tags');
-@define('DASHBAORD_ENTRIES', 'In Progress');
+@define('DASHBOARD_ENTRIES', 'In Progress');
