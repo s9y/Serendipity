eeb25b2b66eb11bca66afa33f265e963c2ceaceb
Ian <aristophian@googlemail.com>
2015-02-05 15:46:53 +0100

    fix fallback for custom template emoticons
    adds an example file to extend this to custom emoticons
    Add description of this in plugins config.

--- plugins/serendipity_event_emoticate/lang_en.inc.php
+++ plugins/serendipity_event_emoticate/lang_en.inc.php
@@ -1,4 +1,4 @@
-<?php #
+<?php
 
 /**
  *  @version 
@@ -11,4 +11,6 @@
 @define('PLUGIN_EVENT_EMOTICATE_TRANSFORM', 'Standard emoticons like :-) and ;-) are converted to images.');
 @define('PLUGIN_EVENT_EMOTICATE_EXTENSION', 'File extension');
 @define('PLUGIN_EVENT_EMOTICATE_EXTENSION_BLAHBLAH', 'The file extension of your emoticons. This is case sensitive.');
+
+@define('PLUGIN_EVENT_EMOTICATE_EXAMPLE_EXTEND_DESC', 'You may extend this by your own added emoticon images in "/templates/yourTemplate/img/emoticons/*" and a configuration file "emoticons.inc.php" added to your templates root. A copy example of this file is added in this plugin directory here. The additional serendipity_event_emoticonchooser plugin extends this to get a clickable list of alle defined emoticons where possible.');
 
