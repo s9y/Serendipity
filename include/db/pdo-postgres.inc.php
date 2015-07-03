c6031dc1dc8f5df8bff11c1f283773c3d4b698af
Ian <aristophian@googlemail.com>
2015-07-03 17:18:23 +0200

    remove $Ids$

--- include/db/pdo-postgres.inc.php
+++ include/db/pdo-postgres.inc.php
@@ -1,4 +1,5 @@
-<?php # $Id: postgres.inc.php 1178 2006-05-01 13:53:40Z garvinhicking $
+<?php
+# $Id: postgres.inc.php 1178 2006-05-01 13:53:40Z garvinhicking $
 # Copyright (c) 2003-2005, Jannis Hermanns (on behalf the Serendipity Developer Team)
 # All rights reserved.  See LICENSE file for licensing details
 
