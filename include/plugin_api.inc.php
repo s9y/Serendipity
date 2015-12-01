7fd42fde05afd138354153810f4fb2d036f71f25
Ian <aristophian@googlemail.com>
2015-12-01 11:39:13 +0100

    fix typo
    there are more.... of this

--- include/plugin_api.inc.php
+++ include/plugin_api.inc.php
@@ -1523,7 +1523,7 @@ class serendipity_plugin
         if (is_null($_res)) {
             // A protected plugin by a specific owner may not have its values stored in $serendipity
             // because of the special authorid. To display such contents, we need to fetch it
-            // seperately from the DB.
+            // separately from the DB.
             $_res = serendipity_get_user_config_var($this->instance . '/' . $name, null, $defaultvalue);
         }
 
