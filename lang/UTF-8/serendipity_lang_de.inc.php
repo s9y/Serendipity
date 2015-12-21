b98117f821e1aed8d72cb763921abf97cafa18a9
Ian <aristophian@googlemail.com>
2015-12-21 19:20:57 +0100

    fix lang typo

--- lang/UTF-8/serendipity_lang_de.inc.php
+++ lang/UTF-8/serendipity_lang_de.inc.php
@@ -685,7 +685,7 @@
 
 @define('BACKEND_TITLE', 'Zusätzlicher Informationstext, der auf der Plugin-Oberfläche dargestellt wird');
 @define('BACKEND_TITLE_FOR_NUGGET', 'An dieser Stelle können Sie einen eigenen Text angeben, der auf der Plugin-Konfigurationsoberfläche zusätzlich angezeigt wird. Denn falls Sie mehrere HTML-Klötze mit leerem Titel angelegt haben, ist es gegebenenfalls schwierig, diese auseinanderzuhalten.');
-@define('CATEGORIES_ALLOW_SELECT', 'Besuchern erlauben, mehrere Kategorien gleichzeit darzustellen?');
+@define('CATEGORIES_ALLOW_SELECT', 'Besuchern erlauben, mehrere Kategorien gleichzeitig darzustellen?');
 @define('CATEGORIES_ALLOW_SELECT_DESC', 'Falls diese Option aktiviert ist, wird eine Auswahlbox neben jeder Kategorie angezeigt, damit Besucher mehrere Kategorien gleichzeitig auswählen können.');
 @define('PAGE_BROWSE_PLUGINS', 'Seite %s von %s, insgesamt %s Plugins.');
 @define('INSTALL_CAT_PERMALINKS', 'Permalinks');
