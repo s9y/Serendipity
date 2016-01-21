96316701e4bbe8b2b745410af5ec3add647560f3
Ian <aristophian@googlemail.com>
2016-01-21 18:46:50 +0100

    remove some \t

--- plugins/serendipity_plugin_creativecommons/lang_tn.inc.php
+++ plugins/serendipity_plugin_creativecommons/lang_tn.inc.php
@@ -2,7 +2,7 @@
 ##########################################################################
 # Copyright (c) 2003-2005, Jannis Hermanns (on behalf the Serendipity    #
 # Developer Team) All rights reserved.  See LICENSE file for licensing   #
-# details								                                 #
+# details                                                                #
 #                                                                        #
 # (c) 2003 Jannis Hermanns <J@hacked.it>                                 #
 # http://www.jannis.to/programming/serendipity.html                      #
@@ -14,3 +14,4 @@
 
 @define('PLUGIN_SIDEBAR_CREATIVECOMMONS_NAME', 'CC 授權條款');
 @define('PLUGIN_SIDEBAR_CREATIVECOMMONS_DESC', '在網誌顯示 CC 的授權條款。');
+
