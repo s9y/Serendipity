197edb27305330cff0d9333dedd01e76c9745945
Ian <aristophian@googlemail.com>
2016-01-26 16:42:27 +0100

    deployment files cleanup

--- deployment/serendipity_admin.php
+++ deployment/serendipity_admin.php
@@ -1,8 +1,9 @@
-<?php # $Id$
+<?php
 # Copyright (c) 2003-2005, Jannis Hermanns (on behalf the Serendipity Developer Team)
 # All rights reserved.  See LICENSE file for licensing details
 # Serendipity is provided in managed mode here
 
 require_once 's9y/serendipity_admin.php';
+
 /* vim: set sts=4 ts=4 expandtab : */
 ?>
--- deployment/serendipity_config.inc.php
+++ deployment/serendipity_config.inc.php
@@ -1,4 +1,4 @@
-<?php # $Id$
+<?php
 # Copyright (c) 2003-2005, Jannis Hermanns (on behalf the Serendipity Developer Team)
 # All rights reserved.  See LICENSE file for licensing details
 # Serendipity is provided in managed mode here
@@ -6,5 +6,6 @@
 #@define('S9Y_INCLUDE_PATH', 's9y/');
 @define('S9Y_DATA_PATH', dirname( __FILE__)."/");
 require_once 's9y/serendipity_config.inc.php';
+
 /* vim: set sts=4 ts=4 expandtab : */
 ?>
