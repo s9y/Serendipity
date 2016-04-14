s9y/Serendipity

commit cc4e9f953e30332109a6860b1a6cf4a2261b8788

Introduce recommended section in theme backend Meant to highlight modern themes for guiding users not to pick the outdated xhtml themes still available via spartacus. See http://board.s9y.org/viewtopic.php?f=5&t=20713

templates/2k11/admin/templates.inc.tpl
@@ -91,10 +91,7 @@
             </article>
         {/if}
 
-        <h2>{$CONST.AVAILABLE_TEMPLATES}</h2>
-
-        <ul class="plainList clearfix">
-        {foreach $templates as $template=>$info}
+        {function name=templateBlock}
             <li><article class="clearfix">
                     <h3 title="{$info.info.name}">{$info.info.name|truncate:25:"&hellip;"}</h3>
                     <div class="clearfix equal_heights template_wrap">
@@ -140,6 +137,21 @@
                 {/if}
                 </article>
             </li>
+        {/function}
+
+        <h2>{$CONST.RECOMMENDED}</h2>
+        <ul class="plainList clearfix">
+        {foreach $recommended_templates as $template=>$info}
+            {templateBlock template=$template info=$info}
+        {/foreach}
+        </ul>
+        
+
+        <h2>{$CONST.AVAILABLE_TEMPLATES}</h2>
+
+        <ul class="plainList clearfix">
+        {foreach $templates as $template=>$info}
+            {templateBlock template=$template info=$info}
         {/foreach}
         </ul>
     </section>

include/admin/templates.inc.php
@@ -175,12 +175,12 @@ $stack = array();
 serendipity_plugin_api::hook_event('backend_templates_fetchlist', $stack);
 $themes = serendipity_fetchTemplates();
 $data['templates'] = array();
+$data['recommended_templates'] = array();
 
 foreach($themes AS $theme) {
     $stack[$theme] = serendipity_fetchTemplateInfo($theme);
 }
 ksort($stack);
-
 
 foreach ($stack as $theme => $info) {
     /* Sorry, but we don't display engines */
@@ -222,12 +222,20 @@ foreach ($stack as $theme => $info) {
         $unmetRequirements[] = 'Serendipity '. $info['require serendipity'];
         $data['templates'][$theme]['unmetRequirements'] = sprintf(UNMET_REQUIREMENTS, implode(', ', $unmetRequirements));
     }
+
+    if ($info['recommended']) {
+        $data['recommended_templates'][$theme] = $data['templates'][$theme];
+        if ($serendipity['template'] != $theme) {
+            unset($data['templates'][$theme]);
+        }
+    }
 }
 
 $data['cur_tpl']         = $data['templates'][$data['cur_template']];
 $data['cur_tpl_backend'] = $data['templates'][$data['cur_template_backend']];
 
 unset($data['templates'][$data['cur_template']]);
+unset($data['recommended_templates'][$data['cur_template']]);
 
 echo serendipity_smarty_show('admin/templates.inc.tpl', $data);
 

commit e84bc1aa8062d93d628a63021b91d843b5e8979c

Fix theme info overlay Broken in cc4e9f953e30332109a6860b1a6cf4a2261b8788 because @key does not work after the variable was moved as an argument into a function

templates/2k11/admin/templates.inc.tpl
@@ -106,7 +106,7 @@
                             {/if}
                         {/if}
 
-                            <footer id="template_info_{$info@key}" class="template_info additional_info">
+                            <footer id="template_info_{$key}" class="template_info additional_info">
                                 <dl class="clearfix">
                                     <dt class="template_author">{$CONST.AUTHOR}:</dt>
                                     <dd>{$info.info.author}</dd>
@@ -121,7 +121,7 @@
                         </div>
                     </div>
 
-                    <button class="template_show_info button_link" type="button" data-href="#template_info_{$info@key}" title="{$CONST.TEMPLATE_INFO}"><span class="icon-info-circled"></span><span class="visuallyhidden"> {$CONST.TEMPLATE_INFO}</span></button>
+                    <button class="template_show_info button_link" type="button" data-href="#template_info_{$key}" title="{$CONST.TEMPLATE_INFO}"><span class="icon-info-circled"></span><span class="visuallyhidden"> {$CONST.TEMPLATE_INFO}</span></button>
                 {if $info.demoURL}
                     <a class="demo_link button_link" href="{$info.demoURL}" title="{$CONST.THEMES_PREVIEW_BLOG}" target="_blank"><span class="icon-search"></span><span class="visuallyhidden"> {$CONST.THEMES_PREVIEW_BLOG}</span></a>
                 {/if}
@@ -142,7 +142,7 @@
         <h2>{$CONST.RECOMMENDED}</h2>
         <ul class="plainList clearfix">
         {foreach $recommended_templates as $template=>$info}
-            {templateBlock template=$template info=$info}
+            {templateBlock template=$template info=$info key=$info@key}
         {/foreach}
         </ul>
         
@@ -151,7 +151,7 @@
 
         <ul class="plainList clearfix">
         {foreach $templates as $template=>$info}
-            {templateBlock template=$template info=$info}
+            {templateBlock template=$template info=$info key=$info@key}
         {/foreach}
         </ul>
     </section>