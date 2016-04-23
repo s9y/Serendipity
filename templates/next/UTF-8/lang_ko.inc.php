e4c333ddedb74d240f0915e64c641ffd16e9050f
Wesley Woo-Duk Hwang-Chung <wesley96@gmail.com>
2016-04-23 21:55:33 +0900

    Korean translation added

---
+++ templates/next/UTF-8/lang_ko.inc.php
@@ -0,0 +1,16 @@
+<?php
+// Theme options
+@define('NEXT_INSTR', '<b>주의:</b> Next가 <em>보기에는</em> 2k11과 사실상 같아보여도 기술적으로는 완전히 다른 템플릿입니다. 그러므로 2k11용 user.css 생성기와는 호환되지 <strong>않습니다</strong>.<br>업데이트에 영향받지 않는 형태로 사용자 스타일을 추가하고자 할 경우 <code>/templates/next/user.css</code> 파일을 추가히시기 바랍니다. 자동으로 세렌디피디가 적용할 것입니다.');
+@define('NEXT_HEADER_IMG','배너 그림을 헤더에 적용하겠습니까? (폭은 1296픽셀, 미적용 시 비워둡니다)');
+@define('NEXT_USE_CORENAV', '전체 내비게이션을 사용하겠습니까?');
+@define('NEXT_WEBFONTS', '구글이 호스팅하는 웹 글꼴을 사용하겠습니까?');
+@define('NEXT_NOWEBFONT', '웹 글꼴 포함시키지 않기');
+@define('NEXT_USERSTYLES', 'user.css 파일을 사용하겠습니까? (템플릿 디렉토리에 해당 파일을 만들어두어야 합니다!)');
+@define('NEXT_REFCOMMENTS', '댓글에 관련 댓글로 이어지는 링크를 추가하겠습니까? (다른 댓글에 대한 답글일 경우)');
+// Lang constants
+@define('NEXT_PLINK_TEXT', '링크');
+@define('NEXT_PLINK_TITLE', '이 댓글에 대한 고정 링크');
+@define('NEXT_PLACE_SEARCH', '단어 검색');
+@define('NEXT_SEND_MAIL', '발송');
+@define('NEXT_REPLYORIGIN', '원래글');
+@define('NEXT_NAVTEXT', '메뉴');
