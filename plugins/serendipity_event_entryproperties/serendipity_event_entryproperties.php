6b1348a7f14fe02b568205b1b14d136f03c5809b
Garvin Hicking <blog@garv.in>
2016-12-23 10:13:40 +0100

    improve hidding password from recent chrome

--- plugins/serendipity_event_entryproperties/serendipity_event_entryproperties.php
+++ plugins/serendipity_event_entryproperties/serendipity_event_entryproperties.php
@@ -356,7 +356,7 @@ class serendipity_event_entryproperties extends serendipity_event
 ?>
             <div class="entryproperties_access_pw adv_opts_box adv_opts_box form_field">
                 <label for="properties_access_pw"><?php echo PASSWORD; ?>:</label>
-                <input type="password" name="ignore_password" value="" style="display:none">
+                <input type="password" name="ignore_password" value="" style="visibility: hidden; width: 1px">
                 <input id="properties_access_pw" name="serendipity[properties][entrypassword]" type="password" autocomplete="off" value="<?php echo serendipity_specialchars($password); ?>">
             </div>
 <?php
