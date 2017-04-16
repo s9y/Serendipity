c43b419939a5acfa600ca4a5a1d9e44b9eb10f08
Matthias Mees <mm@yellowled.de>
2017-04-16 10:11:26 +0200

    Add German translation + UTF-8 lang files

---
+++ plugins/serendipity_event_responsiveimages/lang_de.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ *  @version 1.0
+ *  @author YellowLed
+ *  @translated 2017/04/16
+ */
+
+@define('PLUGIN_EVENT_RESPONSIVE_NAME', 'Responsive Images');
+@define('PLUGIN_EVENT_RESPONSIVE_DESC', 'Skaliert Grafiken mit Hilfe von HTML5 und dem s9y-Backend passend zur Breite des Viewports des Besuchers');
