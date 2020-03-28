261bac42f1049d9b8fb1001b5fa4559418f85a48
Thomas Hochstein <thh@inter.net>
2020-03-28 13:26:06 +0100

    Bump event_spartacus version
    Signed-off-by: Thomas Hochstein <thh@inter.net>

--- plugins/serendipity_event_spartacus/serendipity_event_spartacus.php
+++ plugins/serendipity_event_spartacus/serendipity_event_spartacus.php
@@ -27,7 +27,7 @@ class serendipity_event_spartacus extends serendipity_event
         $propbag->add('description',   PLUGIN_EVENT_SPARTACUS_DESC);
         $propbag->add('stackable',     false);
         $propbag->add('author',        'Garvin Hicking');
-        $propbag->add('version',       '2.38.2');
+        $propbag->add('version',       '2.38.3');
         $propbag->add('requirements',  array(
             'serendipity' => '1.6',
         ));
