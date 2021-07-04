eb38547a243bbc749dd1cc9f01441e03a0ec19fd
onli <onli@paskuda.biz>
2021-07-04 18:43:08 +0200

    Init all required comment form fields

--- include/functions_comments.inc.php
+++ include/functions_comments.inc.php
@@ -204,7 +204,7 @@ function serendipity_displayCommentForm($id, $url = '', $comments = NULL, $data 
         'commentform_entry'          => $entry
     );
 
-    if (! isset($commentform_data['required_fields'])) { $commentform_data['required_fields'] = ['name' => false, 'comment' => false]; }
+    if (! isset($commentform_data['required_fields'])) { $commentform_data['required_fields'] = ['name' => false, 'comment' => false, 'email' => false, 'url' => false]; }
 
     $serendipity['smarty']->assign($commentform_data);
 
