d8d0e12c1f022fa532c49d1ed741ac282f4dc1b5
onli <onli@paskuda.biz>
2024-02-13 19:18:36 +0100

    Remove hard PHP 8.0 requirement This way, s9y still works with PHP 7.4, which makes the upgrade path easier.

--- bundled-libs/composer/autoload_real.php
+++ bundled-libs/composer/autoload_real.php
@@ -35,7 +35,7 @@ class ComposerAutoloaderInitcbda25b16bb8365467298ce193f0f30c
         require __DIR__ . '/autoload_static.php';
         call_user_func(\Composer\Autoload\ComposerStaticInitcbda25b16bb8365467298ce193f0f30c::getInitializer($loader));
 
-        $loader->setApcuPrefix('Z/MU3fsy9VaX7GThuDWyc');
+        $loader->setApcuPrefix('pZXHgfsNRpI3/XO56IexP');
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitcbda25b16bb8365467298ce193f0f30c::$files;
