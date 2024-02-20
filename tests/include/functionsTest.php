e092bea1a3b2b6aaec2a12facf0f3a5a7a960de2
Christian Garbs <mitch@cgarbs.de>
2024-02-20 08:59:13 +0100

    add simple CI pipeline with basic PHP test runner (#823)
    * add initial PHP test workflow
    
    test matrix contains all currently released and supported PHP versions
    
    * actually run the tests as described in tests/README
    
    * tests: fix PHPUnit imports
    
    * tests: fix missing constant S9Y_INCLUDE_PATH
    
    * tests: fix PHPUnit deprecation warning: DataProviders must be static
    
    ```
    2 tests triggered 2 PHPUnit deprecations:
    
    1) functionsTest::test_serendipity_serverOffsetHour
    Data Provider method functionsTest::serverOffsetHourDataProvider() is not static
    
    /home/runner/work/Serendipity/Serendipity/tests/include/functionsTest.php:17
    
    2) functionsTest::test_serendipity_serverOffsetHourWithTimestampNull
    Data Provider method functionsTest::serverOffsetHourWithTimestampNullDataProvider() is not static
    
    /home/runner/work/Serendipity/Serendipity/tests/include/functionsTest.php:46
    ```
    
    * tests: migrate PHPUnit XML configuration to version 11.0
    
    ```
    There was 1 PHPUnit test runner deprecation:
    
    1) Your XML configuration validates against a deprecated schema. Migrate your XML configuration using "--migrate-configuration"!
    ```
    
    * tests: disable code coverage for now
    
    * tests: migrate doc-comments to Attributes
    
    ```
    There were 3 PHPUnit test runner deprecations:
    
    1) Metadata found in doc-comment for method functionsConfigTest::test_serendipity_getTemplateFile(). Metadata in doc-comments is deprecated and will no longer be supported in PHPUnit 12. Update your test code to use attributes instead.
    
    2) Metadata found in doc-comment for method functionsTest::test_serendipity_serverOffsetHour(). Metadata in doc-comments is deprecated and will no longer be supported in PHPUnit 12. Update your test code to use attributes instead.
    
    3) Metadata found in doc-comment for method functionsTest::test_serendipity_serverOffsetHourWithTimestampNull(). Metadata in doc-comments is deprecated and will no longer be supported in PHPUnit 12. Update your test code to use attributes instead.
    
    ```
    
    * tests: fix assertions
    
    `assertContains()` is used for arrays etc., but we only have a string.
    Use `assertStringEndsWith()` instead, the _ends with_ also makes the
    test more robust.
    
    * tests: also run on PHP 7.4

--- tests/include/functionsTest.php
+++ tests/include/functionsTest.php
@@ -2,17 +2,19 @@
 
 $serendipity['dbType'] = 'pdo-sqlite';
 define('IN_serendipity', true);
+define('S9Y_INCLUDE_PATH', dirname(__FILE__) . '/../../');
 require_once dirname(__FILE__) . '/../../include/functions.inc.php';
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
 
 /**
  * Class functionsTest
  */
-class functionsTest extends PHPUnit_Framework_TestCase
+class functionsTest extends PHPUnit\Framework\TestCase
 {
-    /**
-     * @test
-     * @dataProvider serverOffsetHourDataProvider
-     */
+    #[Test]
+    #[DataProvider("serverOffsetHourDataProvider")]
     public function test_serendipity_serverOffsetHour($serverOffsetHours, $timestamp, $negative, $expected)
     {
         global $serendipity;
@@ -24,7 +26,7 @@ class functionsTest extends PHPUnit_Framework_TestCase
     /**
      * @return array
      */
-    public function serverOffsetHourDataProvider()
+    public static function serverOffsetHourDataProvider()
     {
         return array(
             array(0, 0, false, 0),
@@ -38,10 +40,8 @@ class functionsTest extends PHPUnit_Framework_TestCase
         );
     }
 
-    /**
-     * @test
-     * @dataProvider serverOffsetHourWithTimestampNullDataProvider
-     */
+    #[Test]
+    #[DataProvider("serverOffsetHourWithTimestampNullDataProvider")]
     public function test_serendipity_serverOffsetHourWithTimestampNull($serverOffsetHours, $negative)
     {
         global $serendipity;
@@ -62,7 +62,7 @@ class functionsTest extends PHPUnit_Framework_TestCase
     /**
      * @return array
      */
-    public function serverOffsetHourWithTimestampNullDataProvider()
+    public static function serverOffsetHourWithTimestampNullDataProvider()
     {
         return array(
             array(null, false),
