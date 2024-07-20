09efedbc4721d9fb7b7d21a59c0b9cd9694de0a9
Guillaume <guillaume.valadas@gmail.com>
2024-07-20 17:13:39 +0200

    Update regex boolean mode is trigger only on operator followed by words

--- include/functions_entries.inc.php
+++ include/functions_entries.inc.php
@@ -882,7 +882,7 @@ function &serendipity_searchEntries($term, $limit = '', $searchresults = '') {
         $cond['distinct'] = '';
         $term             = str_replace('&quot;', '"', $term);
         $relevance_enabled = true;
-        if (preg_match('@([\+\-\*~<>\(\)"])\s*(\b\w+\b)@', $term)) {
+        if (preg_match('@@[\+\-\*~<>\(\)"].[\S]*@', $term)) {
             $cond['find_part'] = "MATCH(title,body,extended) AGAINST('$term' IN BOOLEAN MODE)";
         } else {
             $cond['find_part'] = "MATCH(title,body,extended) AGAINST('$term')";
