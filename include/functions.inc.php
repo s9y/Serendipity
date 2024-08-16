3734df1293d92a06d20881f12018113eae617005
Thomas Hochstein <thh@thh.name>
2024-08-16 21:33:55 +0200

    serendipity_sendMail: terminate headers with CRLF. (#853)

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -564,7 +564,7 @@ function serendipity_sendMail($to, $subject, $message, $fromMail, $headers = NUL
     }
 
     if (!isset($maildata['skip_native']) && !empty($maildata['to'])) {
-        return mail($maildata['to'], $maildata['subject'], $maildata['message'], implode("\n", $maildata['headers']));
+        return mail($maildata['to'], $maildata['subject'], $maildata['message'], implode("\r\n", $maildata['headers']));
     }
 }
 
