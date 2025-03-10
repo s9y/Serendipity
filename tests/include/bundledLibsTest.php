3b8cead4537ccadedf6f002f3f7d0ab2ed516ac7
onli <onli@paskuda.biz>
2025-03-10 16:24:12 +0100

    XML/RPC: Fix spl_object_hash parser conversion (#875)
    * Replace (int) parser conversion with spl_object_hash
    The (int) conversion does not work anymore since PHP 8 changed the xml parser from being a resource to being an object. Hardcoding it to a number, the initial fix, destroys the logic of having multiple parsers available and was not done everywhere. The object hash approach should be a valid replacement.
    
    * Add test for XML_RPC_Message
    
    * Add test for XML_RPC_Response, when valid
    
    * Add test for invalid XML_Response, simplify code
    
    * Fix $message access
    
    * Try test fix for PHP 7.4
    
    * Try test for PHP 7.4, LANG-CHARSET missing
    
    * PHP 7.4 bc fix for XML_PARSER hash
    
    * Try PHP 7.4 'ha' fix
    
    * Try fix for undefined 'ha' index
    
    * Use ?? check for legibility
    
    Co-authored-by: Garvin Hicking <blog@garv.in>
    
    ---------
    
    Co-authored-by: Garvin Hicking <blog@garv.in>

---
+++ tests/include/bundledLibsTest.php
@@ -0,0 +1,76 @@
+<?php
+
+@define('S9Y_PEAR_PATH', dirname(__FILE__) . '/../../bundled-libs/');
+@define('IN_serendipity', true);
+@define('LANG_CHARSET', 'UTF-8');
+require_once dirname(__FILE__) . '/../../include/compat.inc.php';
+if (!class_exists('XML_RPC_Base')) {
+    include_once(S9Y_PEAR_PATH . "XML/RPC.php");
+}
+
+use PHPUnit\Framework\Attributes\Test;
+
+class bundledLibsTest extends PHPUnit\Framework\TestCase {
+    var $message = null;
+
+    protected function setUp(): void {
+        $args = array(
+            new XML_RPC_Value(
+                'testA',
+                'string'
+            ),
+            new XML_RPC_Value(
+                'testB',
+                'string'
+            )
+        );
+        $this->message = new XML_RPC_Message(
+            'weblogUpdates.ping',
+            $args
+        );
+        $this->message->createPayload();
+    }
+    
+    #[Test]
+    public function test_serendipity_xml_rpc_message()
+    {
+        $expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r
+<methodCall>\r
+<methodName>weblogUpdates.ping</methodName>\r
+<params>\r
+<param>\r
+<value><string>testA</string></value>\r
+</param>\r
+<param>\r
+<value><string>testB</string></value>\r
+</param>\r
+</params>\r
+</methodCall>\r
+";
+        $this->assertEquals($expected, $this->message->payload);
+    }
+
+    #[Test]
+    public function test_serendipity_xml_rpc_response_valid()
+    {
+        $example_response = "<methodResponse>
+<params>
+<param>
+<value><string>Success!</string></value>
+</param>
+</params>
+</methodResponse>";
+
+        $xmlrpc_result = $this->message->parseResponse($example_response);
+        $this->assertEquals($example_response, $xmlrpc_result->serialize());
+    }
+    #[Test]
+    public function test_serendipity_xml_rpc_response_invalid()
+    {
+        $example_response = "some non-xml response";
+
+        $xmlrpc_result = $this->message->parseResponse($example_response);
+        $this->assertEquals(2, $xmlrpc_result->faultCode());
+    }
+
+}
