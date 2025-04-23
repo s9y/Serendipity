940b318d0015b0757b3006d486f7c221979b59ef
onli <onli@paskuda.biz>
2025-04-23 21:09:14 +0200

    Update smarty to 5.4.4 (#889)
    * Update smarty to 5.4.4
    
    * document changes
    
    * update smarty to 5.4.5
    
    * document changes

--- bundled-libs/composer/autoload_real.php
+++ bundled-libs/composer/autoload_real.php
@@ -35,7 +35,7 @@ class ComposerAutoloaderInitcbda25b16bb8365467298ce193f0f30c
         require __DIR__ . '/autoload_static.php';
         call_user_func(\Composer\Autoload\ComposerStaticInitcbda25b16bb8365467298ce193f0f30c::getInitializer($loader));
 
-        $loader->setApcuPrefix('b96dc61e52fff7f3a39b');
+        $loader->setApcuPrefix('12ae3a44a2f45b52e407');
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitcbda25b16bb8365467298ce193f0f30c::$files;
