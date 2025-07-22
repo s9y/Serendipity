2bad84600f06892a6b62b51a51ef6aafd76f7a28
onli <onli@paskuda.biz>
2025-07-22 22:03:15 +0200

    raise version and document changes

--- plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
+++ plugins/serendipity_event_spamblock/serendipity_event_spamblock.php
@@ -27,7 +27,7 @@ class serendipity_event_spamblock extends serendipity_event
             'smarty'      => '2.6.7',
             'php'         => '4.1.0'
         ));
-        $propbag->add('version',       '1.89.6');
+        $propbag->add('version',       '1.89.7');
         $propbag->add('event_hooks',    array(
             'frontend_saveComment' => true,
             'external_plugin'      => true,
