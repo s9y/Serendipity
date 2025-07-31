18a5e97ba0d285603a0e4e9c3f52f9f6450ab360
onli <onli@paskuda.biz>
2025-07-31 18:53:11 +0200

    Prepare repo for 2.6-beta1

--- serendipity_config.inc.php
+++ serendipity_config.inc.php
@@ -54,7 +54,7 @@ if (defined('USE_MEMSNAP')) {
 }
 
 // The version string
-$serendipity['version'] = '2.6-alpha1';
+$serendipity['version'] = '2.6-beta1';
 
 
 // Setting this to 'false' will enable debugging output. All alpha/beta/cvs snapshot versions will emit debug information by default. To increase the debug level (to enable Smarty debugging), set this flag to 'debug'.
