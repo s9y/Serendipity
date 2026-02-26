dfd1726b11e080602aafd209288aaed136e82fe4
onli <onli@paskuda.biz>
2026-02-26 11:50:18 +0100

    Maintenance action clear template cache -> clear also s9y cache The maintenance action to clear the template cache could be misleading when the internal cache was activated, since changes would be delayed.
    This also moved the maintenance code path to a POST action, so it won't be triggered by accident by browser preloading sites. And instead of only deleting some cache files of the current active theme it now deletes all smarty cache files - the argumentation that this would lead to a high memory consumption later when caches for e.g. the gravatar plugin are renewed is not correct anymore, since those caches are not managed by smarty (and smarty won't delete them).
    
    Closes https://github.com/s9y/Serendipity/issues/910

--- include/admin/maintenance.inc.php
+++ include/admin/maintenance.inc.php
@@ -13,31 +13,48 @@ if (! isset($_POST['adminAction'])) {
 }
 
 if ($_POST['adminAction'] == "maintenanceMode") {
-    if (serendipity_db_bool(serendipity_get_config_var("maintenanceMode", false)) === true) {
-        serendipity_set_config_var("maintenanceMode", false);
-    } else {
-        serendipity_set_config_var("maintenanceMode", true);
-        # after the specified time, given in hours from now, we want the maintenance mode to not be active anymore.
-        # This prevents users from logging themselves out.
-        $activeTill = time() + ($_POST['hours'] * 3600);
-        serendipity_set_config_var("maintenanceModeEnd", $activeTill);
+    if (serendipity_checkPermission('siteConfiguration') || serendipity_checkPermission('blogConfiguration')) {
+        if (serendipity_db_bool(serendipity_get_config_var("maintenanceMode", false)) === true) {
+            serendipity_set_config_var("maintenanceMode", false);
+        } else {
+            serendipity_set_config_var("maintenanceMode", true);
+            # after the specified time, given in hours from now, we want the maintenance mode to not be active anymore.
+            # This prevents users from logging themselves out.
+            $activeTill = time() + ($_POST['hours'] * 3600);
+            serendipity_set_config_var("maintenanceModeEnd", $activeTill);
+        }
     }
 }
 
 
 if ($_POST['adminAction'] == 'nativetoutf8') {
-    # TODO: Check rights
-    include S9Y_INCLUDE_PATH . 'include/functions_upgrader.inc.php';
-    serendipity_upgrade_native_utf8();
+    if (serendipity_checkPermission('siteConfiguration') || serendipity_checkPermission('blogConfiguration')) {
+        include S9Y_INCLUDE_PATH . 'include/functions_upgrader.inc.php';
+        serendipity_upgrade_native_utf8();
+    }
 }
 
 if ($_POST['adminAction'] == 'utf8toutf8mb4') {
-    # TODO: Check rights
-    include S9Y_INCLUDE_PATH . 'include/functions_upgrader.inc.php';
-    echo serendipity_upgradeUTF8_UTF8mb4();
+    if (serendipity_checkPermission('siteConfiguration') || serendipity_checkPermission('blogConfiguration')) {
+        include S9Y_INCLUDE_PATH . 'include/functions_upgrader.inc.php';
+        echo serendipity_upgradeUTF8_UTF8mb4();
+    }
 }
 
-switch($serendipity['GET']['adminAction']) {
+if ($_POST['adminAction'] == 'runcleanup') {
+    if (serendipity_checkPermission('adminTemplates')) {
+        // The smarty method clearCompiledTemplate() clears all compiled smarty template files in templates_c. It will
+        // not delete other caches under templates_c/, so we do not need to restrict it to the current template
+        if(method_exists($serendipity['smarty'], 'clearCompiledTemplate')) {
+            $data['cleanup_finish']   = (int)$serendipity['smarty']->clearCompiledTemplate();
+            $data['cleanup_template'] = $serendipity['template'];
+        }
+        serendipity_cleanCache();
+    }
+}
+
+
+switch ($serendipity['GET']['adminAction']) {
     case 'integrity':
         $data['action'] = "integrity";
 
@@ -47,19 +64,6 @@ switch($serendipity['GET']['adminAction']) {
         }
         $data['badsums'] = serendipity_verifyFTPChecksums();
         break;
-
-    case 'runcleanup':
-        // The smarty method clearCompiledTemplate() clears all compiled smarty template files in templates_c
-        // Since there may be other compiled template files in templates_c too, we have to restrict this call() to clear the blogs template only,
-        // to not have the following automated recompile, force the servers memory to get exhausted,
-        // when using plugins like serendipity_event_gravatar plugin, which can eat up some MB...
-        // Restriction to template means: leave the page we are on: ../admin/index.tpl and all others, which are set, included and compiled by runtime. (plugins, etc. this can be quite some..!)
-        if(method_exists($serendipity['smarty'], 'clearCompiledTemplate')) {
-            $data['cleanup_finish']   = (int)$serendipity['smarty']->clearCompiledTemplate(null, $serendipity['template']);
-            $data['cleanup_template'] = $serendipity['template'];
-        }
-        break;
-
 }
 
 
