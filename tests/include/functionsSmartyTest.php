8d8ae5d2f24bebca6775762f4753f4fc79eed674
onli <onli@paskuda.biz>
2026-07-22 21:41:44 +0200

    Set language constant as workaround under PHP 8.1

--- tests/include/functionsSmartyTest.php
+++ tests/include/functionsSmartyTest.php
@@ -51,6 +51,11 @@ class functionsSmartyTest extends PHPUnit\Framework\TestCase
     {
         global $serendipity;
 
+        if (version_compare(PHP_VERSION, '8.1.0', '>') && version_compare(PHP_VERSION, '8.2.0', '<')) {
+            // the strftime polyfill fails in the pipeline because of a missing locale under PHP 8.1
+            @setlocale(LC_TIME, ['de_DE', 'de_US.UTF-8', 'de-DE']);
+        }
+
         $success = serendipity_smarty_init();
 
         $this->assertEquals(true, $success);
